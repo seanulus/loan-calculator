e1fc3558df54c2d66e60631d653ffc4e599f609e
Sean <seanjmac@hotmail.com>
2022-01-23 22:16:28 -0800

    feat: add disqualify page

---
+++ src/components/disqualified/Disqualified.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Disqualified.css'
+
+export default function Disqualified() {
+	return (
+		<div className='disqualified'>
+			<h2>We're sorry. Our calculations have found that you are not qualified for this loan at this time. 
+				Please contact our support team at <a href='tel:555-555-5555'>555-555-5555</a>
+			</h2>
+		</div>
+	)
+}
